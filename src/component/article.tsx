582ac0d3ad1e82480c925189ca965e922be60796
Alfonsus Ardani <alfonsusac@gmail.com>
2025-07-30 08:23:26 +0700

    feat: implement previous and next page navigation in article layout

--- src/component/article.tsx
+++ src/component/article.tsx
@@ -1,6 +1,9 @@
 import type { MDXComponents } from "next-mdx-remote-client"
 import { MDX } from "./mdx"
 import { Breadcrumb } from "./breadcrumb"
+import type { ComponentProps } from "react"
+import { cn } from "lazy-cn"
+import { CardDescription, CardLink, CardTitle } from "./card"
 
 export function ArticleLayoutTemplate(props: {
   children?: React.ReactNode
@@ -20,4 +23,60 @@ export function ArticleLayoutTemplate(props: {
       </section>
     </div>
   )
+}
+
+function getPrevNextPage(slug: string, collection: Record<string, any>) {
+  const keys = Object.keys(collection)
+  const index = keys.indexOf(slug)
+  let nextPage: { page: any, slug: string } | null = null
+  let prevPage: { page: any, slug: string } | null = null
+  if (index > 0) {
+    prevPage = { page: collection[keys[index - 1]], slug: keys[index - 1] }
+  } else {
+    prevPage = null
+  }
+  if (index < keys.length - 1) {
+    nextPage = { page: collection[keys[index + 1]], slug: keys[index + 1] }
+  } else {
+    nextPage = null
+  }
+  return { nextPage, prevPage }
+}
+
+
+export const article = {
+  layout: ArticleLayoutTemplate,
+  getPrevNext: getPrevNextPage,
+  PrevNext: function PrevNext(props: {
+    data: ReturnType<typeof getPrevNextPage>,
+    href: string
+  }) {
+    const { nextPage, prevPage } = props.data
+    return (
+      <div className="my-12 grid grid-cols-2 gap-4">
+        {
+          prevPage ?
+            <CardLink href={`${ props.href }${ prevPage?.slug }`}>
+              <CardDescription>Previous</CardDescription>
+              <CardTitle>
+                {prevPage ? prevPage.page.$title : "No previous page"}
+              </CardTitle>
+            </CardLink>
+            : <div></div>
+
+        }
+        {
+          nextPage ?
+            <CardLink href={`${ props.href }${ nextPage?.slug }`}>
+              <CardDescription>Next</CardDescription>
+              <CardTitle>
+                {nextPage ? nextPage.page.$title : "No next page"}
+              </CardTitle>
+            </CardLink>
+            : <div></div>
+        }
+      </div>
+    )
+  }
+
 }
