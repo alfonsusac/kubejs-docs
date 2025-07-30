aade96aaeb7cced31a3085eddc22e6b16be34da2
Alfonsus Ardani <alfonsusac@gmail.com>
2025-07-30 15:43:36 +0700

    feat: update documentation structure to include tutorial page

--- content/structure.ts
+++ content/structure.ts
@@ -1,10 +1,11 @@
 import { PageGroup, StandalonePage } from "@/lib/docs/docs2"
 import { EventGroupsPage } from "./api/events/+index"
 import { FolderStructurePage } from "./folders/+index"
+import { TutorialPage } from "./guide/+index"
 
 export const docs_structure = [
   PageGroup("Getting Started", [
-    StandalonePage({ href: "/docs/guide", title: "Tutorials", subtitle: "A collection of tutorials to help you get started with KubeJS." }),
+    TutorialPage,
     StandalonePage({ href: "/docs/tips", title: "Tips", subtitle: "A collection of tips and tricks to help you use KubeJS effectively." }),
     FolderStructurePage,
     StandalonePage({ href: "/docs/addons", title: "Addons", subtitle: "All KubeJS mod integrations" }),
